6edb858686597c9b8e213ae7dcb8af06958c7012
WillMidia <63423575+WillMidia@users.noreply.github.com>
2024-08-24 18:24:53 -0300

    Ajustes front-end login

--- android/app/src/main/kotlin/com/example/projeto_perguntas2/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.projeto_perguntas2
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/screens/employee_login_screen.dart
+++ lib/screens/employee_login_screen.dart
@@ -9,74 +9,114 @@ class EmployeeLoginScreen extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: [
-          Positioned.fill(
+          Positioned(
+            top: MediaQuery.of(context).size.height / 2 - 120, // Ajusta para centralizar verticalmente
+            left: MediaQuery.of(context).size.width / 2 - 150, // Ajusta para centralizar horizontalmente
             child: Opacity(
               opacity: 0.1,
-              child: Image.asset(
-                'assets/images/eurofarma_logo.png',
-                fit: BoxFit.cover,
+              child: Container(
+                width: 300,  // Define a largura maior
+                height: 280,  // Define a altura maior
+                child: Image.asset(
+                  'assets/images/eurofarma-back.png',
+                ),
               ),
             ),
           ),
           SafeArea(
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset(
-                    'assets/images/eurofarma_logo.png',
+            child: Column(
+              children: [
+                SizedBox(height: 60), // Adiciona espaço acima do logo
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: Image.asset(
+                    'assets/images/eurofarma-logo.png',
                     width: 200,
                     height: 50,
                   ),
-                  SizedBox(height: 40),
-                  CustomTextField(
-                    hintText: 'LOGIN',
-                  ),
-                  SizedBox(height: 20),
-                  CustomTextField(
-                    hintText: 'SENHA',
-                    obscureText: true,
-                  ),
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: TextButton(
-                      onPressed: () {},
-                      child: Text(
-                        'Esqueci a senha',
-                        style: TextStyle(color: Colors.grey),
+                ),
+                Expanded(
+                  child: Center(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          CustomTextField(
+                            hintText: 'LOGIN',
+                          ),
+                          SizedBox(height: 20),
+                          CustomTextField(
+                            hintText: 'SENHA',
+                            obscureText: true,
+                          ),
+                          Align(
+                            alignment: Alignment.centerRight,
+                            child: TextButton(
+                              onPressed: () {},
+                              child: Text(
+                                'Esqueci a senha',
+                                style: TextStyle(color: Colors.grey),
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 20),
+                          Row(
+                            children: [
+                              Expanded(
+                                child: Container(
+                                  margin: EdgeInsets.symmetric(horizontal: 10),
+                                  child: ElevatedButton(
+                                    onPressed: () {
+                                      Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                          builder: (context) => HomeScreen(),
+                                        ),
+                                      );
+                                    },
+                                    style: ElevatedButton.styleFrom(
+                                      backgroundColor: Colors.blue, // Cor de fundo
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(30), // Bordas arredondadas
+                                      ),
+                                      padding: EdgeInsets.symmetric(vertical: 15),
+                                    ),
+                                    child: Text(
+                                      'ENTRAR',
+                                      style: TextStyle(fontSize: 16),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              SizedBox(width: 20),
+                              Expanded(
+                                child: Container(
+                                  margin: EdgeInsets.symmetric(horizontal: 10),
+                                  child: ElevatedButton(
+                                    onPressed: () {},
+                                    style: ElevatedButton.styleFrom(
+                                      backgroundColor: Colors.grey, // Cor de fundo
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(30), // Bordas arredondadas
+                                      ),
+                                      padding: EdgeInsets.symmetric(vertical: 15),
+                                    ),
+                                    child: Text(
+                                      'REGISTRAR',
+                                      style: TextStyle(fontSize: 16),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
                       ),
                     ),
                   ),
-                  SizedBox(height: 20),
-                  Row(
-                    children: [
-                      Expanded(
-                        child: CustomButton(
-                          text: 'ENTRAR',
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => HomeScreen(),
-                              ),
-                            );
-                          },
-                          color: Colors.blue,
-                        ),
-                      ),
-                      SizedBox(width: 20),
-                      Expanded(
-                        child: CustomButton(
-                          text: 'REGISTRAR',
-                          onPressed: () {},
-                          color: Colors.grey,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ],
