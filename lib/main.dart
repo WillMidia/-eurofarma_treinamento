b77c7fc92ac3d3df7ae4c760327bda62b068694f
willt <will.tedros@gmail.com>
2024-09-26 14:56:04 -0300

    Integração com SQLite local

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,27 @@
 import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
 import 'screens/profile_screen.dart';
 import 'screens/training_screen.dart';
 import 'screens/employee_login_screen.dart';
 import 'screens/rh_login_screen.dart';
-import 'screens/home_screen.dart';
 import 'screens/user_selection_screen.dart';
-import 'screens/register_employee_screen.dart'; // Adicionei o import da tela de cadastro de colaborador
-import 'screens/profile_registration_screen.dart'; // Adicionei o import da tela de cadastro de perfil
+import 'screens/register_employee_screen.dart';
+import 'screens/profile_registration_screen.dart';
+import 'database/database_helper.dart'; // Importa o DatabaseHelper
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Inicializa o banco de dados e adiciona dados de teste se necessário
+  final dbHelper = DatabaseHelper();
+
+  // Insira dados de teste no banco de dados, se necessário
+  await dbHelper.insertTraining({
+    'title': 'Introdução à Farmácia',
+    'description': 'Um treinamento básico sobre farmacologia.',
+    'icon': 'school',
+  });
+
   runApp(MyApp());
 }
 
@@ -29,8 +42,8 @@ class MyApp extends StatelessWidget {
         '/profile': (context) => ProfileScreen(),
         '/training': (context) => TrainingScreen(),
         '/user_selection': (context) => UserSelectionScreen(),
-        '/registerEmployee': (context) => RegisterEmployeeScreen(), // Rota para a tela de registro de colaborador
-        '/profileRegistration': (context) => ProfileRegistrationScreen(), // Rota para a tela de cadastro de perfil
+        '/registerEmployee': (context) => RegisterEmployeeScreen(),
+        '/profileRegistration': (context) => ProfileRegistrationScreen(),
       },
     );
   }
--- lib/screens/training_screen.dart
+++ lib/screens/training_screen.dart
@@ -1,72 +1,46 @@
 import 'package:flutter/material.dart';
-import '../widgets/bottom_nav_screen.dart'; // Certifique-se de importar o novo widget
+import '../widgets/bottom_nav_screen.dart';
+import '../database/database_helper.dart'; // Importa o helper do banco de dados
+import '../models/training_model.dart'; // Importa o modelo de treinamento
 
-class TrainingScreen extends StatelessWidget {
-  final List<Map<String, dynamic>> trainings = [
-    {
-      'title': 'Introdução à Farmácia',
-      'description': 'Aprenda os fundamentos básicos da farmácia, incluindo ética e legislação.',
-      'icon': Icons.school
-    },
-    {
-      'title': 'Manipulação de Medicamentos',
-      'description': 'Curso sobre a preparação e manipulação segura de medicamentos.',
-      'icon': Icons.medication
-    },
-    {
-      'title': 'Atendimento ao Cliente',
-      'description': 'Técnicas para oferecer um atendimento de qualidade e resolver problemas comuns.',
-      'icon': Icons.person
-    },
-    {
-      'title': 'Gestão de Inventário',
-      'description': 'Métodos eficazes para gerenciar e controlar o estoque de medicamentos.',
-      'icon': Icons.inventory
-    },
-    {
-      'title': 'Farmacologia Básica',
-      'description': 'Conhecimentos fundamentais sobre farmacologia e como os medicamentos atuam.',
-      'icon': Icons.local_pharmacy
-    },
-    {
-      'title': 'Segurança e Compliance',
-      'description': 'Práticas para garantir a segurança e conformidade nas operações farmacêuticas.',
-      'icon': Icons.lock
-    },
-    {
-      'title': 'Farmacocinética',
-      'description': 'Estudo sobre como o corpo absorve, distribui, metaboliza e excreta medicamentos.',
-      'icon': Icons.biotech
-    },
-    {
-      'title': 'Toxicologia',
-      'description': 'Análise dos efeitos adversos de substâncias químicas no organismo.',
-      'icon': Icons.warning
-    },
-    {
-      'title': 'Legislação Farmacêutica',
-      'description': 'Aspectos legais e regulamentares da profissão farmacêutica.',
-      'icon': Icons.gavel
-    },
-  ];
+class TrainingScreen extends StatefulWidget {
+  @override
+  _TrainingScreenState createState() => _TrainingScreenState();
+}
+
+class _TrainingScreenState extends State<TrainingScreen> {
+  late Future<List<Training>> _trainings;
+
+  @override
+  void initState() {
+    super.initState();
+    _trainings = _fetchTrainings();
+  }
+
+  Future<List<Training>> _fetchTrainings() async {
+    final dbHelper = DatabaseHelper();
+    final List<Map<String, dynamic>> trainingMaps = await dbHelper.getTrainings();
+    return List.generate(trainingMaps.length, (i) {
+      return Training.fromMap(trainingMaps[i]);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return BottomNavScreen(
       child: Stack(
-        fit: StackFit.expand, // Expande o Stack para ocupar toda a área disponível
+        fit: StackFit.expand,
         children: [
-          // Imagem de fundo
           Align(
             alignment: Alignment.center,
             child: Opacity(
-              opacity: 0.1, // Ajusta a transparência
+              opacity: 0.1,
               child: Container(
-                width: 450, // Define a largura da imagem
-                height: 450, // Define a altura da imagem
+                width: 450,
+                height: 450,
                 child: Image.asset(
-                  'assets/images/eurofarma-back.png', // Substitua pelo nome correto da imagem
-                  fit: BoxFit.cover, // Ajusta a imagem para cobrir o container
+                  'assets/images/eurofarma-back.png',
+                  fit: BoxFit.cover,
                 ),
               ),
             ),
@@ -82,24 +56,39 @@ class TrainingScreen extends StatelessWidget {
                 ),
                 SizedBox(height: 20),
                 Expanded(
-                  child: ListView.builder(
-                    itemCount: trainings.length,
-                    itemBuilder: (context, index) {
-                      return TrainingCard(
-                        title: trainings[index]['title'],
-                        description: trainings[index]['description'],
-                        icon: trainings[index]['icon'],
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => TrainingDetailsScreen(
-                                trainingTitle: trainings[index]['title'],
-                              ),
-                            ),
-                          );
-                        },
-                      );
+                  child: FutureBuilder<List<Training>>(
+                    future: _trainings,
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return Center(child: CircularProgressIndicator());
+                      } else if (snapshot.hasError) {
+                        return Center(child: Text('Erro ao carregar dados'));
+                      } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                        return Center(child: Text('Nenhum treinamento disponível'));
+                      } else {
+                        final trainings = snapshot.data!;
+                        return ListView.builder(
+                          itemCount: trainings.length,
+                          itemBuilder: (context, index) {
+                            final training = trainings[index];
+                            return TrainingCard(
+                              title: training.title,
+                              description: training.description,
+                              icon: Icons.school, // Ajuste conforme necessário
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => TrainingDetailsScreen(
+                                      trainingTitle: training.title,
+                                    ),
+                                  ),
+                                );
+                              },
+                            );
+                          },
+                        );
+                      }
                     },
                   ),
                 ),
@@ -177,42 +166,12 @@ class TrainingDetailsScreen extends StatelessWidget {
             if (trainingTitle == 'Introdução à Farmácia') ...[
               SizedBox(height: 10),
               Text(
-                'A farmácia é uma ciência dedicada ao estudo dos medicamentos e à sua aplicação para a promoção da saúde. O curso de Introdução à Farmácia abrange uma variedade de tópicos essenciais que fornecem uma base sólida para o estudo mais aprofundado da profissão.',
+                'A farmácia é uma ciência dedicada ao estudo dos medicamentos e à sua aplicação para a promoção da saúde.',
                 style: TextStyle(fontSize: 16),
               ),
-              SizedBox(height: 20),
-              Text(
-                'Conteúdo do Curso:',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 10),
-              Text(
-                '• História da Farmácia\n'
-                    '• Princípios da Farmacologia\n'
-                    '• Ética e Legislação Farmacêutica\n'
-                    '• Tipos de Medicamentos e Seus Usos\n'
-                    '• Sistemas de Saúde e Papel do Farmacêutico\n',
-                style: TextStyle(fontSize: 16, height: 1.5),
-              ),
-              SizedBox(height: 5),
-              Image.asset(
-                'assets/images/farmacia.jpg', // Atualize para o nome e caminho corretos da imagem
-                fit: BoxFit.cover,
-              ),
-              SizedBox(height: 20),
-              Text(
-                'Referências:',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 10),
-              Text(
-                '• Livro: Introdução à Farmácia por Autor X\n'
-                    '• Artigo: Fundamentos da Farmácia por Autor Y\n',
-                style: TextStyle(fontSize: 16, height: 1.5),
-              ),
-              SizedBox(height: 20),
+              // Adicione mais conteúdo aqui
             ],
-            // Adicione outros conteúdos aqui para outros treinamentos se necessário
+            // Adicione mais trechos de conteúdo para outras opções de treinamento
           ],
         ),
       ),
